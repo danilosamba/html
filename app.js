f700b237bd6e01fe7e23529bfb56238791d0e143
Danilo Miranda <dh.miranda.dm@gmail.com>
2024-09-09 20:37:06 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,16 @@
+function mostrar(){
+let n1 = document.getElementById('campo1').value
+let n2 = document.getElementById('campo2').value
+//let total = document.getElementById('resultado')
+if (n1 == '' || n2 == ''){
+    alert('Preencha algum número nos dois campos para continuar')
+    return;
+} else{
+    let resultado = document.getElementById('resultado');
+    let total = parseInt(n1) + parseInt(n2);
+    resultado.innerHTML = `O total da soma é ${total}`
+    console.log(`Número n1 ${n1} número n2 ${n2} total ${total}`)
+}
+//alert(`O texto digitado é ${n1}`)
+}
+
